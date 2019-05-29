3584419b429f3dcf38c39e5bbe95adc311daad0d
Kelsey T Williams <ckelseytheewilliams@gmail.com>
2019-05-29 14:02:47 -0700

    first commit of bangbang_bot

---
+++ README.md
@@ -0,0 +1,19 @@
+#BangBang bot
+
+	Bang Bang bot is an autoclicker designed to play the flash first person shooter game, Bloody Day in Chrome.  Once running, the bot takes over the role of the player with shooting accuracy as good as or better than a person.  The name of the bot comes from it's filename on my dad's computer when I was a kid.  Personally, I'd like to know when exactly it stopped being Bang Bang and magically became Bloody Day but whatever, it's called bang bang bot now.
+
+###Prerequisites
+
+	In order to use Bang Bang bot, python 3x is required and pyautogui installed.  Pyautogui can be installed with the command `pip install pyautogui`.
+
+###Using BangBang bot
+
+	To install and use, download the repository or copy the contents of the `bin` folder and place them into a single directory.  [Start the flash game up in chrome.](http://www.bngames.com/games/bloody-day-1/full_screen.html). Then run the program, for example `python your\directory\bin\bangbang.py`, from the command line.  To quit the application, enter `Ctrl + c` in the command line.
+
+###Author
+
+- Kelsey Williams
+
+###License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ bin/bangbang.py
@@ -0,0 +1,40 @@
+'''Autoclicker to play the game bloody day http://www.bngames.com/games/bloody-day-1/full_screen.html.
+Works best in full screen at 100% zoom in chrome.'''
+
+import pyautogui
+
+while True:
+
+    # Bool to determine direction of target
+    right = False
+    left = False
+
+    # Target detection logic
+    im = pyautogui.screenshot(region=(200, 400, 800, 350))
+    target = pyautogui.locate('assets\right.png', im, grayscale=False, confidence = 0.5)
+    target2 = pyautogui.locate('assets\left.png', im, grayscale=False, confidence = 0.5)
+
+    # Two if blocks are used to determine the direction the target is movement
+    # Right moving target handler
+    if target != None and left == False:
+        right = True
+    # In order to compensate for computational delay, numbers are added to the targets pixel position
+        x_r = target[0]+200 
+        y_r = target[1]+400
+    # Firing loop
+        for i in range(11):
+            # Because of processing, overhead, and target speed the numbers added to x and y are fined tuned to current code.
+            # Its reccomended these be left as are or changed and tuned again if the code is altered.
+            pyautogui.click(x_r +185+(i*5), y_r+20)
+        pyautogui.moveTo(736, 519)    
+        right = False
+    # Left moving target handler
+    if target2 != None and right == False:
+        left = True
+        x_l = target2[0]+200
+        y_l = target2[1]+400
+        if target2[0] - 185 -50 > 0:
+            for i in range(11):
+                pyautogui.click(x_l-185-(i*5), y_l+20)
+            pyautogui.moveTo(736, 519)
+            left = False
